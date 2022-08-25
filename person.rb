4fd3836160bd0004cf9f773963bf3ef47b6acf56
“ksigei” <sigeikiprono4@gmail.com>
2022-08-25 14:59:23 +0300

    Solve errors

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(name = 'Unknown', age, parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @name = name
     @age = age
     @parent_permission = parent_permission
--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(name = 'Unknown', age, classroom, parent_permission: true)
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(name, age, parent_permission)
     @classroom = classroom
   end
