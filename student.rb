8f368ec082e0e19793f3f111a0d86c62c21d24ac
“ksigei” <sigeikiprono4@gmail.com>
2022-08-25 23:13:22 +0300

    OOP school library: decorate a class

--- student.rb
+++ student.rb
@@ -3,12 +3,28 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
-    super(name, age, parent_permission)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(name, age, parent_permission: parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
-    '¯\(ツ)/¯'
+    "¯\(ツ)/¯"
   end
 end
+
+# new student
+student = Student.new('John', 18, '1A', parent_permission: true)
+# check student
+puts student.name
+# check student
+puts student.age
+student.classroom = '1A'
+# check student
+puts student.classroom
+# check student
+puts student.can_use_services?
+# check student
+puts student.play_hooky
+# check student
+puts student.of_age?
