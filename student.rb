b1a871e8e16986698225ab498b1bc83cc1b79ec8
“ksigei” <sigeikiprono4@gmail.com>
2022-08-26 13:29:35 +0300

    Solve linter errors

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(name, age, parent_permission: parent_permission)
     @classroom = classroom
   end
